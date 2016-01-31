110cbe5d3835712796848e20004a7600e79387e0
ph3ng <ph3ng@mail.fresnostate.edu>
2016-01-31 09:43:36 -0800

    surfer

---
+++ 04_surfer page/serve.go
@@ -0,0 +1,29 @@
+package main
+import (
+      "html/template"
+      "net/http"
+      "log"
+)
+
+func surfer(res http.ResponseWriter, req *http.Request)  {
+    tmp, err := template.ParseFiles("./index.html")
+    if err != nil {
+        log.Fatalln(err)
+    }
+    tmp.Execute(res, nil)
+}
+
+/*func img(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "pics/surf.jpg")
+}
+
+func back(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "css/main.css")
+}
+*/
+
+func main() {
+    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+    http.HandleFunc("/", surfer)
+    http.ListenAndServe(":8080", nil)
+}
