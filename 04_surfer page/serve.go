623e5ed56a00d73a86a6f25d324f3e4544314bfa
ph3ng <ph3ng@mail.fresnostate.edu>
2016-02-05 12:54:28 -0800

    stuff

--- 04_surfer page/serve.go
+++ 04_surfer page/serve.go
@@ -18,6 +18,11 @@ func surfer(res http.ResponseWriter, req *http.Request)  {
 func main() {
     http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
     http.Handle("/pic/", http.StripPrefix("/pic/", http.FileServer(http.Dir("pic"))))
+    //serving css and img through two statements
+
+    ////http.Handle("/public/",http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+    //serving css and img from one folder that contains both files.
+
     http.HandleFunc("/", surfer)
     http.ListenAndServe(":8080", nil)
 }
