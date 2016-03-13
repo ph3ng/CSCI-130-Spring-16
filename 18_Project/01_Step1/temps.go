084477db6cabbbaa2cc605d9ad9d5e3114ea18fe
ph3ng <ph3ng@mail.fresnostate.edu>
2016-03-13 13:16:17 -0700

    updates

--- 18_Project/01_Step1/temps.go
+++ 18_Project/01_Step1/temps.go
@@ -9,19 +9,13 @@ import (
 func anything(res http.ResponseWriter, req *http.Request) {
 
 	temp, err := template.ParseFiles("templates/temps.html")
-	logFatalError(err)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	temp.Execute(res, nil)
-	logFatalError(err)
 }
 func main() {
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.HandleFunc("/", anything)
 	http.ListenAndServe(":8080", nil)
-}
-
-// Logs error at Fatal level given the error is not nil.
-func logFatalError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
