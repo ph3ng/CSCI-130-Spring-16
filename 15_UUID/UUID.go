a29c2cfd048e564e5e1b7cc96a6c8d910d3eb26b
ph3ng <ph3ng@mail.fresnostate.edu>
2016-03-13 12:43:59 -0700

    ...

---
+++ 15_UUID/UUID.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nu7hatch/gouuid"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+
+	cookie, err := req.Cookie("session-id")
+
+	if err != nil {
+		id, _ := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:  "session-id",
+			Value: id.String(),
+			// Secure: true,
+			HttpOnly: true,
+		}
+		http.SetCookie(res, cookie)
+	}
+	fmt.Println(cookie)
+
+}
