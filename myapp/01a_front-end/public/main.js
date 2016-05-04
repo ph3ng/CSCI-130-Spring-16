a19b85196fe7204097396b074f4703b3a46f0656
ph3ng <ph3ng@mail.fresnostate.edu>
2016-05-03 20:03:49 -0700

    done

--- myapp/01a_front-end/public/main.js
+++ myapp/01a_front-end/public/main.js
@@ -3,5 +3,18 @@ var el = document.querySelector
 
 el.addEventListener("input",
   function(e) {
-    console.log(e.target.value);
+    //creat AJAX request
+    var xhr = new XMLHttpRequest();
+
+    //send AJAX request
+    xhr.open("post", "/api/check");
+    console.log("sending:", e.target.value);
+    xhr.send(e.target.value);
+
+    //receive AJAX response
+    xhr.addEventListener("readystatechange",function(){
+      if (xhr.readyState  === 4 && xhr.status === 200) {
+        console.log("receieved from server", xhr.responseText);
+      }
+    })
   })
