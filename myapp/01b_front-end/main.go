ced4545219809f9aaffa2cb8c99151236a01ffe2
ph3ng <ph3ng@mail.fresnostate.edu>
2016-05-09 10:45:39 -0700

    step 4

--- myapp/01b_front-end/main.go
+++ myapp/01b_front-end/main.go
@@ -1,16 +1,21 @@
-package entrychecker
+package project
 
-import "html/template"
+import (
+	"html/template"
+	"io"
+	"net/http"
 
-import "net/http"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+)
 
 var tpl *template.Template
 
+const gcsBucket = "team_project_bucket.appspot.com"
+
 func init() {
 	http.HandleFunc("/", index)
-	// http.HandleFunc("/api/check", checker)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
-
 	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css/"))))
 	http.Handle("/img/", http.StripPrefix("/img", http.FileServer(http.Dir("img/"))))
 	tpl = template.Must(template.ParseGlob("*.html"))
@@ -20,14 +25,51 @@ func index(res http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(res, "index.html", nil)
 }
 
-// func checker(res http.ResponseWriter, req *http.Request) {
-// 	ctx := appengine.NewContext(req)
-//
-// 	bs, err := ioutil.ReadAll(req.Body)
-// 	if err != nil {
-// 		http.Error(res, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// 	log.Infof(ctx, "received %v", string(bs))
-// 	io.WriteString(res, "we received"+string(bs))
-// }
+func handler(res http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+
+	html := `
+	    <form method="POST" enctype="multipart/form-data">
+		<input type="file" name="dahui">
+		<input type="submit">
+	    </form>
+	`
+
+	if req.Method == "POST" {
+
+		mpf, hdr, err := req.FormFile("dahui")
+		if err != nil {
+			log.Errorf(ctx, "ERROR handler req.FormFile: ", err)
+			http.Error(res, "We were unable to upload your file\n", http.StatusInternalServerError)
+			return
+		}
+		defer mpf.Close()
+
+		fname, err := uploadFile(req, mpf, hdr)
+		if err != nil {
+			log.Errorf(ctx, "ERROR handler uploadFile: ", err)
+			http.Error(res, "We were unable to accept your file\n"+err.Error(), http.StatusUnsupportedMediaType)
+			return
+		}
+
+		fnames, err := putCookie(res, req, fname)
+		if err != nil {
+			log.Errorf(ctx, "ERROR handler putCookie: ", err)
+			http.Error(res, "We were unable to accept your file\n"+err.Error(), http.StatusUnsupportedMediaType)
+			return
+		}
+
+		html += `<h1>Files</h1>`
+		for k, _ := range fnames {
+			html += `<h3>` + k + `</h3>`
+		}
+	}
+
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(res, html)
+}
